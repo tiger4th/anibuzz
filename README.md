58c058f0dce3737664fa1c92aee1330c0f7c3cfc
tiger4th <tiger4th@gmail.com>
2014-10-21 17:29:23 +0900

    stable

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 anibuzz
 =======
 
-anibuzz
+anibuzz  
+http://tiger4th.com/anibuzz/
---
+++ ad.php
@@ -0,0 +1,179 @@
+<div class="drop-shadow curved curved-all">
+<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=tiger4th-22&o=9&p=9&l=st1&mode=dvd-jp&search=<?php $n = mt_rand(1, 3); $s = explode(" ", $anime[$search[$n]][0]); echo $s[0]; ?>&fc1=000000&lt1=_blank&lc1=3366FF&bg1=FFFFFF&f=ifr" marginwidth="0" marginheight="0" width="180" height="150" border="0" frameborder="0" style="border:none;" scrolling="no"></iframe>
+</div>
+
+<?php switch ($cour) { ?>
+<?php case 201207: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/4048672185/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51TNmbMOaVL.jpg" alt="境界線上のホライゾン1〈上〉―GENESISシリーズ (電撃文庫)" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/4758070555/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51WfRbS5xsL.jpg" alt="ゆるゆり 1 (IDコミックス 百合姫コミックス)" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007PBNKK4/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/61KxXDJx2OL.jpg" alt="薄桜鬼 黎明録 名残り草 (通常版)予約特典ドラマCD付き" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201204: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007T1L728/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51R09TylVUL.jpg" alt="エウレカセブンAO 1 (初回限定版) [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007WB5EEM/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51c1-2onxPL.jpg" alt="坂道のアポロン 第1巻 Blu-ray 【初回限定生産版】" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007WYJVYS/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/61%2BiyGMGPTL.jpg" alt="聖闘士星矢Ω 1 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007RC1KWQ/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51QsYu1OA5L.jpg" alt="氷菓 限定版 第1巻 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007T6IM0I/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51ii8YQgv0L.jpg" alt="つり球 1【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007SBFRIO/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51HJuGyB1XL.jpg" alt="君と僕。2 1(完全生産限定版) [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B007PNRC06/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/512--ZZHnXL.jpg" alt="アクセル・ワールド 1(初回限定版) [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201201: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0066UPMHE/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51rs-H8jJXL.jpg" alt="夏目友人帳 肆　1【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B006HYN2SU/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/41UOy8FAM2L.jpg" alt="アクエリオンEVOL Vol.1 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B006L16Z7E/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51b-ws4eWpL.jpg" alt="Another 限定版 第1巻 [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201110: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B005M0IKTG/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/510RGPplhFL.jpg" alt="「侵略!? イカ娘① 【初回限定特典（侵略者ステッカー／クリアシートコレクション）付き】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B005XTDFKK/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/615i5brB4VL.jpg" alt="『ラストエグザイル-銀翼のファム-』 Blu-ray No.01 【初回限定生産版：村田蓮爾設定画集（ファム/ジゼル）／プレミアムDVD付き】" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B005WO7WE6/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51EdduvlzFL.jpg" alt="ギルティクラウン 1【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B005IUACPK/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51R0lxsWBDL.jpg" alt="ペルソナ4　1　【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201107: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B005DOXA8W/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51BumlqJLiL.jpg" alt="輪るピングドラム　１（期間限定版） [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0055X2MQW/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/61p1xUdkBAL.jpg" alt="魔乳秘剣帖 ディレクターズカット版 1 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0056D6U28/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51ZJvuLz-oL.jpg" alt="夏目友人帳　参 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201104: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004UO66S2/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/510CuCPrT3L.jpg" alt="青の祓魔師 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004WC0L24/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51ZCMv9DygL.jpg" alt="あの日見た花の名前を僕達はまだ知らない。 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004TK5YOE/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51yqzllVUpL.jpg" alt="STEINS;GATE Vol.1【初回限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0055B4XHK/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51k3Bl7Gh4L.jpg" alt="SKET DANCE　フジサキデラックス版 1　（特典CD付　初回生産限定） [DVD]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004VQ6TJU/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51NMwhH6ZZL.jpg" alt="DOG DAYS 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004U8QOGC/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51Xk5W5rxuL.jpg" alt="花咲くいろは　1 [DVD]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004OR2C4M/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51VHIBPb1IL.jpg" alt="TIGER&amp;BUNNY(タイガー&amp;バニー) 1 (初回限定版)  [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201101: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004INGZAE/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51kDXEQIAnL.jpg" alt="魔法少女まどか☆マギカ 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004I5BSJA/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/516QI-s7L8L.jpg" alt="IS ＜インフィニット・ストラトス＞　第1巻 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004JLO5VQ/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/61tu-fnd4gL.jpg" alt="GOSICK-ゴシック-BD版 第1巻 [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201010: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0042ESY1W/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/61XIqGLIpCL.jpg" alt="侵略！イカ娘1【初回限定特典（1分の1スケール触手切り落とし&amp;缶バッジ）付き】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B00479W5UI/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/517MC7-YlfL.jpg" alt="とある魔術の禁書目録II 第1巻 〈初回限定版〉 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0045SOPWM/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51vIzuUXrEL.jpg" alt="俺の妹がこんなに可愛いわけがない 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B004AS3UNM/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51cPsMrtJWL.jpg" alt="バクマン。1 〈初回限定版〉 [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201007: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003UF9WGA/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/515U0qsPhgL.jpg" alt="アマガミSS 1 森島はるか 上巻 (Blu-ray 初回限定生産)" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003VI9NI8/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51OkPnkyzuL.jpg" alt="世紀末 オカルト学院 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003UDPE6O/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/511KfgdPuBL.jpg" alt="黒執事 II 1 【完全生産限定版】 [DVD]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201004: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003FG9L4C/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51kP%2BoUcmGL.jpg" alt="けいおん!!(第2期) 1 (Blu-ray 初回限定生産) [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003B2THEY/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51IdHLNckSL.jpg" alt="WORKING!! 1 【完全生産限定版】 [DVD]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003FZ8PWW/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/510KoljSM%2BL.jpg" alt="薄桜鬼 第一巻 [DVD]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003FZ0M0K/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51BH7JgJhXL.jpg" alt="Angel Beats! 1 【完全生産限定版】 [Blu-ray]" width="180" /></a>
+</div>
+<?php break; ?>
+
+<?php case 201001: ?>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B003EQOKYY/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/61Ovxe3Wv5L.jpg" alt="ハートキャッチプリキュア!【1】 [DVD]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B00340YWRK/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51YC0A2zMqL.jpg" alt="バカとテストと召喚獣 第1巻 [Blu-ray]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+<a href="http://www.amazon.co.jp/exec/obidos/ASIN/B0039HI4FO/tiger4th-22/ref=nosim/" target="_blank" rel="nofollow"><img src="http://ecx.images-amazon.com/images/I/51tkIYZ-CyL.jpg" alt="うんこさん 純情派 [DVD]" width="180" /></a>
+</div>
+<div class="drop-shadow curved curved-all">
+
+</div>
+<?php break; ?>
+
+<?php default: ?>
+<div class="krkr drop-shadow curved curved-all">
+<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab" id="Player_c3d82327-7ab6-479b-b513-c859da1ad4b4"  WIDTH="160px" HEIGHT="600px"> <PARAM NAME="movie" VALUE="http://ws.amazon.co.jp/widgets/q?rt=tf_cw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Ftiger4th-22%2F8010%2Fc3d82327-7ab6-479b-b513-c859da1ad4b4&Operation=GetDisplayTemplate"><PARAM NAME="quality" VALUE="high"><PARAM NAME="bgcolor" VALUE="#FFFFFF"><PARAM NAME="allowscriptaccess" VALUE="always"><embed src="http://ws.amazon.co.jp/widgets/q?rt=tf_cw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Ftiger4th-22%2F8010%2Fc3d82327-7ab6-479b-b513-c859da1ad4b4&Operation=GetDisplayTemplate" id="Player_c3d82327-7ab6-479b-b513-c859da1ad4b4" quality="high" bgcolor="#ffffff" name="Player_c3d82327-7ab6-479b-b513-c859da1ad4b4" allowscriptaccess="always"  type="application/x-shockwave-flash" align="middle" height="600px" width="160px"></embed></OBJECT> <NOSCRIPT><A HREF="http://ws.amazon.co.jp/widgets/q?rt=tf_cw&ServiceVersion=20070822&MarketPlace=JP&ID=V20070822%2FJP%2Ftiger4th-22%2F8010%2Fc3d82327-7ab6-479b-b513-c859da1ad4b4&Operation=NoScript">Amazon.co.jp ウィジェット</A></NOSCRIPT>
+<br /><br />
+</div>
+<?php } ?>
